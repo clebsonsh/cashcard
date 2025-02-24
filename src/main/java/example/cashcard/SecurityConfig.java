472375439117d0cf3f475520f707b98e755a18b7
Clebson Moura <clebson.sh@gmail.com>
2025-02-23 22:23:39 -0300

    Add a new test user

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -30,7 +30,13 @@ class SecurityConfig {
                 .roles("NON-OWNER")
                 .build();
 
-        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards);
+        UserDetails kumar = users
+                .username("kumar2")
+                .password(passwordEncoder.encode("xyz789"))
+                .roles("CARD-OWNER")
+                .build();
+
+        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards, kumar);
     }
 
     @Bean
